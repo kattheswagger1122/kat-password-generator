786b9744c433f3599a11cd405de5dea10f6fadac
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-05-07 13:02:30 -0400

    debug and change layout

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Assignment Code
 // array variables
 var generateBtn = document.querySelector("#generate");
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberChar = "0123456789";
-var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
+var lowercaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*","(", ")", "_", "-", "+", "=", "{", "}", "[", "]", ";", ":", "'", "`", "~", "<", ">", ".", "?", "/", "|"]
 
 // declaration
 var passwordLength;
@@ -107,7 +107,7 @@ function determineSpecial(){
 
 // random number generator
 function generatePassword(){
-    determineLength();
+    determinePassword();
     console.log(passwordLength);
     determineUppercase();
     console.log(uppercaseCheck);
